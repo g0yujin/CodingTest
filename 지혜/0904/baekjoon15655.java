29b9f8192491962ce07e104cb55c294173a0a45e
Jihye <jihyeeee511@gmail.com>
2024-09-04 08:06:10 +0900

    백준 백트레킹

---
+++ 지혜/0904/baekjoon15655.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class baekjoon15655{
+    static int n,m;
+    static int[] value;
+    static int[] arr;
+    static StringBuilder sb =new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        value = new int[n];
+        arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(value);
+        dfs(0,0);
+        System.out.println(sb);
+    }
+    public static void dfs(int start, int depth){
+        if(depth == m){
+            for(int i =0; i<m; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i =start; i<n; i++){
+            arr[depth] = value[i];
+            dfs(i+1,depth+1);
+        }
+    }
+
+}
