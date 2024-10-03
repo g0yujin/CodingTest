d24a6038334d827f9f48e9d9019122c898e37e70
g0yujin <dbwbsqhd016@gmail.com>
2024-10-03 18:10:47 +0900

    백준 다이나믹 프로그래밍

--- 유진/1002_백준 다이나믹 프로그래밍/2579_계단오르기.py
+++ 유진/1002_백준 다이나믹 프로그래밍/2579_계단오르기.py
@@ -2,16 +2,22 @@ import sys
 
 n = int(sys.stdin.readline()) # 계단의 개수
 
-stairs = [0] * 301
-for i in range(1, n + 1):
-    stairs[i] = int(input())
+stairs = []
+for _ in range(n):
+    stairs.append(int(sys.stdin.readline()))
 
-dp = [0] * 301
-dp[1] = stairs[1]
-dp[2] = stairs[1] + stairs[2]
-dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
+d = [0] * (n+1)
 
-for i in range(4, n + 1):
-    dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i])
+d[1] = stairs[0]
+d[n] = stairs[-1]
 
-print(dp[n])
+if n > 1:
+    d[2] = stairs[0] + stairs[1]
+
+for i in range(n+1, 3, -1):
+    if i % 3 == 0:
+        d[i] = max(d[i-1], d[i-2] + stairs[i-1])
+    else:
+        d[i] = d[i-1] + stairs[i-1]
+
+print(d[n])
