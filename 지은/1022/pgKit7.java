ee6d964b18cbe052b0b50ad9d110865ec1056633
JiELee1 <dlwldmscjstk@naver.com>
2024-10-22 11:09:16 +0900

    코드 수정

--- 지은/1022/pgKit7.java
+++ 지은/1022/pgKit7.java
@@ -2,33 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        boolean[] check = new boolean[progresses.length];
         int cur = 0;
-        boolean end = false;
+        int sum = 0;
         List<Integer> result = new ArrayList<>();
-        while(!end) {
+        while(sum < progresses.length) {
             int day = (int) Math.ceil((100.0-progresses[cur])/speeds[cur]);
             //System.out.println("현재 완료까지 걸리는 일 : "+day);
-            check[cur] = true;
             int count = 1;
             for(int i=cur+1; i<progresses.length; i++) {
                 //System.out.println("현재 i : "+i+", 작업진도 : "+progresses[i]+ ", 일 : "+day);
-                if(!check[i] && progresses[i]+speeds[i]*day>=100) {
-                    check[i]=true;
-                    count++;
-                } else if(progresses[i]+speeds[i]*day<100) break;
+                if(progresses[i]+speeds[i]*day<100) {
+                    cur = i;
+                    break;
+                }
+                count++;
             }
             //System.out.println("count : "+count);
             result.add(count);
-            boolean b = true;
-            for(int i = 0; i<check.length; i++) {
-                if(!check[i]) {
-                    cur = i;
-                    b = false;
-                    break;
-                }
-            }
-            if(b) end = true;
+            sum += count;
         }
         return result.stream().mapToInt(i->i).toArray();
     }
