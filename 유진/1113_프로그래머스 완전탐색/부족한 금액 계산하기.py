29485fc045c63ba686320bd0836fd1cb09d40371
g0yujin <dbwbsqhd016@gmail.com>
2024-11-13 09:09:37 +0900

    프로그래머스 완전탐색

---
+++ 유진/1113_프로그래머스 완전탐색/부족한 금액 계산하기.py
@@ -0,0 +1,9 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(price, count * price + 1, price):
+        total += i
+
+    if total > money:
+        return total - money
+    else:
+        return 0
